068b17d40002d0346f1436179fdb9fc28bb52133
Mark Hudnall <me@markhudnall.com>
2020-02-27 22:53:10 -0800

    Add functions for plotting

--- src/flows/hpgl.clj
+++ src/flows/hpgl.clj
@@ -1,11 +1,15 @@
 (ns flows.hpgl
   (:import [hpglgraphics HPGLGraphics])
-  (:require [quil.applet :as ap]
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.pprint :refer [pprint]]
+            [quil.applet :as ap]
             [quil.core :as q]
             [quil.util :as u]))
 
 (def doc-width 11040)
 (def doc-height 7721)
+(def plotter-conf {:bin "/Users/mark/Documents/code/plot/plot.py"
+                   :padding 1500})
 
 (defn create-hpgl-graphics [w h path]
   (.createGraphics (ap/current-applet) (int w) (int h)
@@ -13,6 +17,7 @@
                    (u/absolute-path path)))
 
 (defn do-record [w h outfile f]
+  "Record HPGL instructions into outfile. w and h are scaled preserving aspect ratio to fit a 11040x7721 plotter, since that's what the underlying implementation, HPGLGraphics, expects. This can be further scaled by the plotter / plot.py using IP and SC instructions."
   (let [scale-val (if (> (/ w h) (/ doc-width doc-height))
                     (/ doc-width w)
                     (/ doc-height h))
@@ -20,10 +25,19 @@
         new-h (* h scale-val)
         padding-w (/ (- doc-width new-w) 2)
         padding-h (/ (- doc-height new-h) 2)] 
-    (println "scaling:" (float scale-val) "new-w:" (float new-w) "new-h:" (float new-h))
-    (println "padding:" (float padding-w) (float padding-h))
     (q/do-record (create-hpgl-graphics doc-width doc-height outfile)
                  (q/translate padding-w padding-h)
                  (q/scale scale-val scale-val)
                  (f))))
 
+
+(defn plot [outfile]
+  (let [outfile (u/absolute-path outfile)
+        {:keys [bin padding]} plotter-conf]
+    (println "Plotting...")
+    (let [result (sh bin :env (merge {"HPGL_FILE" outfile "PADDING" padding}
+                                     (System/getenv)))]
+      (when (contains? result :err)
+        (println "== ERROR ==")
+        (println (:err result)))
+      (println (:out result)))))
