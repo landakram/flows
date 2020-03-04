dd2fb6644331b00695e75dd0bff4f4fd5282921b
Mark Hudnall <me@markhudnall.com>
2020-03-03 20:10:14 -0800

    Use grid2d
    Turns out I want a grid with sequential rows / columns that I can scale up

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :plugins [[lein-localrepo "0.5.4"]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [quil "3.1.0"]
+                 [grid2d "0.1.0-SNAPSHOT"]
                  [plotter-utils "0.1.1"]])
--- src/flows/core.clj
+++ src/flows/core.clj
@@ -1,10 +1,23 @@
 (ns flows.core
   (:require [quil.core :refer :all]
-            [plotter-utils.grid :refer :all]
+            #_[plotter-utils.grid :refer :all]
             [plotter-utils.quil :as pl]
+            [data.grid2d :as grid]
             [quil.helpers.calc :refer [mul-add]]
             [clojure.pprint :refer [pprint]]
             [quil.middleware :as m]))
+
+(defn make-grid [origin width height tile-width tile-height]
+  (let [columns (/ width tile-width)
+        rows (/ height tile-height)]
+    {:g (grid/create-grid columns rows
+                     (fn [[x y]]
+                       {:noise (noise x y 0)}))
+     :width width
+     :height height
+     :tile-width tile-width
+     :tile-height tile-height
+     :origin origin}))
 
 (defn initial-state []
   (let [width (width)
@@ -17,13 +30,27 @@
      :x-start (random 10)
      :y-start (random 10)}))
 
+(defn enumerate-cells [{:keys [g tile-height tile-width]}]
+  (for [[x y] (grid/posis g)]
+    [[(* x tile-width) (* y tile-height)] (get g [x y])]))
+
 (defn setup []
   (frame-rate 60)
   (color-mode :hsb)
   (initial-state))
 
-(defn update-state [state]
-  state)
+(defn update-grid [{:keys [g] :as grid} x-start y-start]
+  (let [transform-fn (fn [[x y] val]
+                       (let [x-noise (mul-add x 0.030 x-start)
+                             y-noise (mul-add y 0.030 y-start)
+                             z-noise (* (frame-count) 0.030)]
+                         (assoc val :noise (noise x-noise y-noise z-noise))))]
+    (-> grid
+        (update :g #(grid/transform % transform-fn)))))
+
+(defn update-state [{:keys [x-start y-start] :as state}]
+  (-> state
+      (update :grid #(update-grid % x-start y-start))))
 
 (defn draw-line [x y width height noise-factor]
   (push-matrix)
@@ -43,27 +70,19 @@
     (pop-matrix)))
 
 (defn draw-flows [{:keys [x-start y-start grid]}]
-  (let [{:keys [tile-width tile-height]} grid
-        grid-points (grid-points grid)]
-    (doseq [[x y] grid-points]
-      (let [x-noise (mul-add x 0.0030 x-start)
-            y-noise (mul-add y 0.0030 y-start)
-            noise-factor (noise
-                          x-noise
-                          y-noise
-                          (*
-                           (frame-count) 0.04))]
-        (draw-line x y
-                   tile-width
-                   tile-height
-                   noise-factor)))))
+  (let [{:keys [tile-width tile-height]} grid]
+    (doseq [[[x y] {:keys [noise]}] (enumerate-cells grid)]
+      (draw-line x y
+                 tile-width
+                 tile-height
+                 noise))))
 
 (defn draw-state [{:keys [x-start y-start grid] :as state}]
   #_(background 255)
   (background 31 31 20)
   
-  #_(draw-flows state)
-  (do
+  (draw-flows state)
+  #_(do
     (let [out "generated/out.hpgl"]
       (pl/do-record
        (width)
