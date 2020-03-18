0d23022b624291c0d448366b7aa0e9076894f329
Mark Hudnall <me@markhudnall.com>
2020-03-17 23:42:25 -0700

    Distort the angles around randomly placed circles

--- src/flows/core.clj
+++ src/flows/core.clj
@@ -7,6 +7,8 @@
             [clojure.pprint :refer [pprint]]
             [quil.middleware :as m]))
 
+(def println pprint)
+
 (defn transform-fn [x-start y-start]
   (fn [[x y] val]
     (let [x-noise (mul-add x 0.030 x-start)
@@ -32,35 +34,80 @@
    :step-length step-length
    :steps steps})
 
-(defn distort-angle [[x y] angle]
-  (let [rect {:x1 200 :y1 200 :x2 200 :y2 650}
-        {:keys [x1 y1 x2 y2]} rect]
+(defn circle-point [theta {:keys [r x y]}]
+  [(+ x (* r (cos theta)))
+   (+ y (* r (sin theta)))])
+
+(defn tan-line-slope [theta circle]
+  (let [next-point (circle-point (+ theta 0.1) circle)
+        prev-point (circle-point (- theta 0.1) circle)
+        dx (- (first next-point) (first prev-point))
+        dy (- (last next-point) (last prev-point))]
+    (when-not (= (int dx) 0)
+      (/ dy dx))))
+
+(defn distance-from-perim [[x y] circle]
+  (let [dx (- x (:x circle))
+        dy (- y (:y circle))]
+    (- (pow (:r circle) 2)
+       (+ (pow dx 2)
+          (pow dy 2)))))
+
+(defn in-circle? [[x y] circle]
+  (>= (distance-from-perim [x y] circle)
+      0))
+
+(defn theta-for-line-tangent-to-circ [dx dy circle]
+  (if (= (int dx) 0)
+    0
+    (let [slope (/ dy dx)
+          theta (atan slope)
+          tan-line-slope (tan-line-slope theta circle)]
+      (if (nil? tan-line-slope)
+        (/ PI 2)
+        (let [tan-line-theta (atan tan-line-slope)]
+          tan-line-theta)))))
+
+(defn in-some-circle [circles [x y]]
+  (first (filter #(in-circle? [x y] %) circles)))
+
+(defn distort-angle [[x y] distortions angle]
+  (let [circle (in-some-circle distortions [x y])]
     (cond
-      (and (= 200)
-           (= 200))
-      (and (> x x1)
-           (< x x2)
-           (> y y1)
-           (< y y2))
-      (* TWO-PI -0.12)
+      (some? circle)
+      (let [r (:r circle)
+            dx (- x (:x circle))
+            dy (- y (:y circle))
+            distance-from-perim (abs (distance-from-perim [x y] circle))
+            theta (mod (theta-for-line-tangent-to-circ dx dy circle) TWO-PI)
+            other-direction-theta (mod (- theta PI) TWO-PI)
+            new-angle (if (> (abs (- angle theta)) (abs (- angle other-direction-theta)))
+                        other-direction-theta
+                        theta)
+            percent-dist-from-perim (/ distance-from-perim (pow r 2))
+            max-dist 0.5
+            new-angle-weight (min 1 (/ percent-dist-from-perim max-dist))]
+        (+ (* new-angle new-angle-weight)
+           (* angle (- 1 new-angle-weight))))
       :else
       angle)))
 
-(defn get-angle [[x y] {:keys [g tile-width tile-height] :as grid}]
+(defn get-angle [[x y] distortions {:keys [g tile-width tile-height] :as grid}]
   (let [col (int (/ x tile-width))
         row (int (/ y tile-height))
         cell (get g [col row])]
     (when-not (nil? cell)
       (->> (* (:noise cell) TWO-PI)
-           (distort-angle [x y])))))
+           (distort-angle [x y] distortions)))))
 
 (defn enumerate-segments [{:keys [origin step-length steps]}
+                          distortions
                           {:keys [g tile-width tile-height] :as grid}]
   (let [[x y] origin]
     (loop [last-x x
            last-y y
            segments []]
-      (let [angle (or (get-angle [last-x last-y] grid)
+      (let [angle (or (get-angle [last-x last-y] distortions grid)
                       -1)
             x-step (* step-length (cos angle))
             y-step (* step-length (sin angle))
@@ -74,6 +121,18 @@
            y
            (conj segments [x y])))))))
 
+(defn random-points [w h]
+  (repeatedly (fn [] [(rand-int w) (rand-int h)])))
+
+(defn random-curve [w h step-length steps distortions]
+  (let [origin (first (drop-while #(some? (in-some-circle distortions %)) (random-points w h)))]
+    (make-curve origin step-length steps)))
+
+(defn random-distortions [min-x max-x min-y max-y min-r max-r]
+  (repeatedly (fn [] {:x (random min-x max-x)
+                      :y (random min-y max-y)
+                      :r (random min-r max-r)})))
+
 (defn initial-state []
   (let [w (* (width) 1.5)
         h (* (height) 1.5)
@@ -83,9 +142,12 @@
                 (- (/ (- h (height)) 2))]
         x-start (random 10)
         y-start (random 10)
+        distortions (take (rand-nth (range 1 10))
+                          (random-distortions 0 (width) 0 (height) 50 150))
         grid (make-grid origin w h tile-width tile-height x-start y-start)]
     {:grid grid
-     :curves (take 1000 (repeatedly #(make-curve [(rand-int w) (rand-int h)] 10 30)))
+     :distortions distortions
+     :curves (take 2500 (repeatedly #(random-curve w h 10 30 distortions)))
      :x-start x-start
      :y-start y-start}))
 
@@ -107,24 +169,26 @@
   (-> state
       (update :grid #(update-grid % x-start y-start))))
 
-(defn draw-line [x y width height noise-factor]
+(defn draw-line [x y width height angle]
   (push-matrix)
   (translate x y)
-  (rotate (* noise-factor TWO-PI))
-  (let [h (map-range noise-factor 0.0 1.0 0 360)
+  (rotate angle)
+  (let [h 100
         s 300
         b 360 #_(map-range noise-factor 0.0 1.0 0 300)
-        alpha (map-range noise-factor 0 1 100 255)
-        length (+ width 20)
+        ;;alpha (map-range noise-factor 0 1 100 255)
+        length (+ width 0)
         start (- (/ length 2))
         end (/ length 2)]
-    (stroke-weight 3)
-    #_(stroke 0 0 0)
-    (stroke h s b alpha)
+    (stroke-weight 1)
+    (stroke 0 0 0)
+    #_(stroke h s b alpha)
     (line start 0 end 0)
+    (fill 0)
+    (ellipse end 0 4 4)
     (pop-matrix)))
 
-(defn draw-curve [curve grid]
+(defn draw-curve [curve distortions grid]
   (let [{:keys [origin]} curve
         [x y] origin
         {:keys [tile-height tile-width]} grid
@@ -137,31 +201,37 @@
     #_(stroke 0)
     #_(stroke h 300 360)
     (begin-shape)
-    (doseq [[x y] (enumerate-segments curve grid)]
+    (doseq [[x y] (enumerate-segments curve distortions grid)]
       (vertex x y))
     (end-shape)
     #_(pop-style)))
 
-(defn draw-flows [{:keys [x-start y-start grid curves]}]
+(defn draw-flows [{:keys [x-start y-start distortions grid curves]}]
   (let [{:keys [tile-width tile-height]} grid]
     (doseq [curve curves]
-      (draw-curve curve grid))
-    #_(doseq [[[x y] {:keys [noise]}] (enumerate-cells grid)]
-      (draw-line x y
-                 tile-width
-                 tile-height
-                 noise))))
+      (draw-curve curve distortions grid))
 
-(defn draw-state [{:keys [x-start y-start grid] :as state}]
+    #_(doseq [[[x y] {:keys [noise]}] (enumerate-cells grid)]
+      (let [angle (get-angle [x y] distortions grid)]
+        (draw-line x y
+                   tile-width
+                   tile-height
+                   angle)))))
+
+(defn draw-state [{:keys [x-start y-start distortions grid] :as state}]
   (background 255)
   #_(background 31 31 20)
   
   (no-fill)
 
+  #_(doseq [{:keys [x y r]} distortions]
+    (ellipse x y (* r 2) (* r 2)))
+  
   (draw-flows state)
+  (save "generated/curve-distortions-3.png")
   (no-loop)
   #_(save-frame "generated/curve-####.png")
-  (do
+  #_(do
     (let [out "generated/out.hpgl"]
       (pl/do-record
        (width)
